075ff2ece2ae192339ff6a356e0837046b6b9eb1
xx <xx>
2018-12-15 04:54:45 +0800

    manager_web与sellergoods_service 联合运行 √

---
+++ pyg_manager_web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -0,0 +1,22 @@
+package com.pinyougou.manager.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.TbBrandService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+    @Reference//注入dubbo容器中的service对象
+    private TbBrandService tbBrandService;
+
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll() {
+        return tbBrandService.findAll();
+    }
+}
