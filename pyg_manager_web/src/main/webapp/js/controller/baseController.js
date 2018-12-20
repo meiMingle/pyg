7030df6b44f9218a75ff2e0049d89554048f9938
xx <xx>
2018-12-20 21:51:37 +0800

    AngularJS分层

---
+++ pyg_manager_web/src/main/webapp/js/controller/baseController.js
@@ -0,0 +1,43 @@
+app.controller('baseController', function ($scope) {
+
+    // 分页控件配置
+    // currentPage：当前页；totalItems：总记录数；itemsPerPage：每页记录数；perPageOptions：分页选项；onChange：当页码变更后自动触发的方法
+    $scope.paginationConf = {
+        currentPage: 1,
+        totalItems: 10,
+        itemsPerPage: 10,
+        perPageOptions: [10, 20, 30, 40, 50],
+        onChange: function () {
+            $scope.reloadList();//重新加载
+        }
+    };
+
+    //重新加载列表 数据
+    $scope.reloadList = function () {
+        //切换页码
+        $scope.findPage($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
+        $scope.selectIds = [];//切换页码清空复选框
+    };
+
+
+    $scope.selectIds = [];
+    //复选框
+    $scope.selectOptions = function ($event, id) {
+        if ($event.target.checked) {
+            $scope.selectIds.push(id);
+        } else {
+            var i = $scope.selectIds.indexOf(id);
+            $scope.selectIds.splice(i, 1);
+        }
+    };
+
+    //全选复选框
+    /*
+                $scope.selectAllOptions = function ($event) {
+                    // alert($event.target.checked);
+
+                };
+                todo : 品牌管理 删除的全选全不选
+    */
+
+});
--- pyg_manager_web/src/main/webapp/js/controller/brandController.js
+++ pyg_manager_web/src/main/webapp/js/controller/brandController.js
@@ -1,26 +1,12 @@
-app.controller('brandController', function ($scope, $http, brandService) {
+app.controller('brandController', function ($scope, $http, brandService, $controller) {
+    //继承父类控制器及其$scope模型
+    $controller('baseController', {$scope: $scope});
+
+
     $scope.findAll = function () {
         brandService.findAll().success(function (res) {
             $scope.list = res;
         });
-    };
-    // 分页控件配置
-    // currentPage：当前页；totalItems：总记录数；itemsPerPage：每页记录数；perPageOptions：分页选项；onChange：当页码变更后自动触发的方法
-    $scope.paginationConf = {
-        currentPage: 1,
-        totalItems: 10,
-        itemsPerPage: 10,
-        perPageOptions: [10, 20, 30, 40, 50],
-        onChange: function () {
-            $scope.reloadList();//重新加载
-        }
-    };
-
-    //重新加载列表 数据
-    $scope.reloadList = function () {
-        //切换页码
-        $scope.findPage($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
-        $scope.selectIds = [];//切换页码清空复选框
     };
 
     $scope.findPage = function (page, size) {
@@ -31,6 +17,7 @@ app.controller('brandController', function ($scope, $http, brandService) {
             }
         );
     };
+
 
     //entity对象初始化
     $scope.entity = {};
@@ -52,25 +39,6 @@ app.controller('brandController', function ($scope, $http, brandService) {
         });
     };
 
-    $scope.selectIds = [];
-    //复选框
-    $scope.selectOptions = function ($event, id) {
-        if ($event.target.checked) {
-            $scope.selectIds.push(id);
-        } else {
-            var i = $scope.selectIds.indexOf(id);
-            $scope.selectIds.splice(i, 1);
-        }
-    };
-
-    //全选复选框
-    /*
-                $scope.selectAllOptions = function ($event) {
-                    // alert($event.target.checked);
-
-                };
-                todo : 品牌管理 删除的全选全不选
-    */
 
     /*删除*/
     $scope.delete = function () {
